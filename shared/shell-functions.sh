93c21edde31b03049385c6e880d19566e32573ea
mi-skam <maksim@miskam.xyz>
2023-10-24 18:06:15 +0200

    adding update to hm

--- shared/shell-functions.sh
+++ shared/shell-functions.sh
@@ -1,9 +1,4 @@
-hm() {
-  if [ "$#" -ne 1 ]; then
-    echo "Usage: hm [switch|edit]"
-    return 1
-  fi
-
+_get_os() {
   os=$(uname -o)
   case "$os" in
   "GNU/Linux")
@@ -17,6 +12,31 @@ hm() {
     exit 1
     ;;
   esac
+}
+
+_handle_update() {
+    dir="$HOME/.config/home-manager"
+    echo "Updating in directory: $dir"
+    
+    # Change to the specified directory
+    pushd "$dir" || { echo "Failed to change to directory: $dir"; return 1; }
+
+    # Perform git pull
+    git pull || { echo "Failed to execute git pull"; return 1; }
+
+    # Perform nix flake
+    nix flake update || { echo "Failed to execute nix flake"; return 1; }
+    
+    popd
+}
+
+hm() {
+  if [ "$#" -ne 1 ]; then
+    echo "Usage: hm [switch|edit|update]"
+    return 1
+  fi
+
+  _get_os
 
   case "$1" in
   "switch")
@@ -26,8 +46,11 @@ hm() {
   "edit")
     eval $EDITOR $HOME/.config/home-manager
     ;;
+  "update")
+    _handle_update
+    ;;
   *)
-    echo "Usage: hm [switch|edit]"
+    echo "Usage: hm [switch|edit|update]"
     return 1
     ;;
   esac
