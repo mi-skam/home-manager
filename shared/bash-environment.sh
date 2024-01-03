7c0c6d15909dc9f4b6126f4a6556543dee616308
mi-skam <maksim@miskam.xyz>
2024-01-03 22:05:34 +0100

    adding podman-bin to path

--- shared/bash-environment.sh
+++ shared/bash-environment.sh
@@ -1,1 +1,1 @@
-export PATH="$HOME/.bun/bin:$HOME/.npm-global/bin:$HOME/.local/bin:/mnt/c/Users/plumps/bin:$PATH"
+export PATH="$HOME/.bun/bin:$HOME/.npm-global/bin:$HOME/.local/bin:/mnt/c/Users/plumps/bin:/opt/podman/bin:$PATH"
